2fbabc983ddbaafa97c93a7013b557074ff185a6
Kaiserr999 <I.camelin74@gmail.com>
2026-04-08 03:53:09 +0200

    Refactor binary_tree_print function to improve tree printing logic and remove unused print_t function

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -3,46 +3,6 @@
 #include <string.h>
 #include "binary_trees.h"
 
-/* Printing function from Holberton School */
-/**
- * print_t - Traverse tree printing nodes with positions
- * @tree: Tree node
- * @offset: Offset
- * @depth: Depth
- * @s: String
- */
-static void print_t(const binary_tree_t *tree, int offset, int depth, char *s)
-{
-	char b[6];
-	int width, left, right, is_left;
-	int i;
-
-	if (!tree)
-		return;
-	is_left = (tree->parent && tree->parent->left == tree);
-	width = sprintf(b, "(%03d)", tree->n);
-	left = (is_left ? (offset - width) / 2 : offset + 2);
-	right = (is_left ? (offset + width) / 2 : offset + width - 1);
-	sprintf(s + left, "%s", b);
-	if (depth)
-	{
-		for (i = left + width / 2; i < right; i++)
-			sprintf(s + i, "-");
-		if (is_left)
-		{
-			sprintf(s + (left + width / 2), "|");
-			print_t(tree->left, left + width / 2 - 1, depth - 1, s - 256);
-			print_t(tree->right, left + width / 2 + 1, depth - 1, s + 256);
-		}
-		else
-		{
-			sprintf(s + (right), "|");
-			print_t(tree->left, right - 1, depth - 1, s - 256);
-			print_t(tree->right, right + 1, depth - 1, s + 256);
-		}
-	}
-}
-
 /**
  * binary_tree_print - Prints a binary tree
  * @tree: Pointer to the root node of the tree to print
@@ -51,17 +11,16 @@ static void print_t(const binary_tree_t *tree, int offset, int depth, char *s)
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
-	char *s;
-	size_t height;
-
 	if (!tree)
 		return;
-	height = 0;
-	s = malloc(sizeof(char) * 255);
-	if (!s)
-		return;
-	memset(s, 32, 255);
-	print_t(tree, 0, height, s);
-	printf("%s\n", s);
-	free(s);
+
+	if (!tree->parent)
+		printf("       %03d\n", tree->n);
+	else if (tree->parent->left == tree)
+		printf("%03d -- %03d\n", tree->parent->n, tree->n);
+	else
+		printf("%03d ++ %03d\n", tree->parent->n, tree->n);
+
+	binary_tree_print(tree->left);
+	binary_tree_print(tree->right);
 }
